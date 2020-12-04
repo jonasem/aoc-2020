3096334ee73f4cc38be9ef1ca338263b0f2247f7
jonas <sirjonas@gmail.com>
2020-12-04 22:11:55 +0100

    Day 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace aoc_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"input/day1");
+            var numbers = lines
+                .Select(line => Convert.ToInt32(line))
+                .OrderBy(number => number);
+
+            var match = numbers.First(number => numbers
+                .Where(pairedNumber => pairedNumber <= 2020 - number)
+                .Any(pairedNumber => number == 2020 - pairedNumber));
+
+            var pairedNumber = 2020 - match;
+
+            Console.WriteLine($"{match} og {pairedNumber}, produkt = {match * pairedNumber} ");
+
+        }
+    }
+}

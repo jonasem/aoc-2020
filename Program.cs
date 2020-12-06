facaf8bd3ae7d50493d25d3a3aafb02474069fc1
jonas <sirjonas@gmail.com>
2020-12-06 18:05:53 +0100

    Day 2

--- Program.cs
+++ Program.cs
@@ -2,23 +2,4 @@
 using System.IO;
 using System.Linq;
 
-var lines = File.ReadAllLines(@"input/day1");
-var numbers = lines.Select(line => Convert.ToInt32(line)).OrderBy(number => number);
-
-var match = numbers.First(number => HasMatch(numbers, 2020 - number));
-var secondMatch = FindMatch(numbers, 2020 - match);
-var thirdMatch = 2020 - match - secondMatch;
-
-Console.WriteLine($"{match} og {secondMatch} og {thirdMatch}, produkt = {match * secondMatch * thirdMatch} ");
-
-
-static bool HasMatch(IOrderedEnumerable<int> numbers, int target) {
-    return numbers.Any(number => numbers
-    .Any(pairedNumber => number + pairedNumber == target));
-}
-
-static int FindMatch(IOrderedEnumerable<int> numbers, int target) {
-    return numbers.FirstOrDefault(number => numbers
-    .Any(pairedNumber => number + pairedNumber == target));
-}
-
+Console.WriteLine("Day 2");

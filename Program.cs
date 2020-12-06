6b6ee0e94dbf7649a9571a6848cf5526a93496a0
jonas <sirjonas@gmail.com>
2020-12-06 18:04:33 +0100

    Use simple entrypoint

--- Program.cs
+++ Program.cs
@@ -2,32 +2,23 @@
 using System.IO;
 using System.Linq;
 
-namespace aoc_2020
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var lines = File.ReadAllLines(@"input/day1");
-            var numbers = lines.Select(line => Convert.ToInt32(line)).OrderBy(number => number);
+var lines = File.ReadAllLines(@"input/day1");
+var numbers = lines.Select(line => Convert.ToInt32(line)).OrderBy(number => number);
 
-            var match = numbers.First(number => HasMatch(numbers, 2020 - number));
-            var secondMatch = FindMatch(numbers, 2020 - match);
-            var thirdMatch = 2020 - match - secondMatch;
+var match = numbers.First(number => HasMatch(numbers, 2020 - number));
+var secondMatch = FindMatch(numbers, 2020 - match);
+var thirdMatch = 2020 - match - secondMatch;
 
-            Console.WriteLine($"{match} og {secondMatch} og {thirdMatch}, produkt = {match * secondMatch * thirdMatch} ");
-        }
+Console.WriteLine($"{match} og {secondMatch} og {thirdMatch}, produkt = {match * secondMatch * thirdMatch} ");
 
 
-        static bool HasMatch(IOrderedEnumerable<int> numbers, int target) {
-            return numbers.Any(number => numbers
-                .Any(pairedNumber => number + pairedNumber == target));
-        }
-
-        static int FindMatch(IOrderedEnumerable<int> numbers, int target) {
-            return numbers.FirstOrDefault(number => numbers
-                .Any(pairedNumber => number + pairedNumber == target));
-        }
-
-    }
+static bool HasMatch(IOrderedEnumerable<int> numbers, int target) {
+    return numbers.Any(number => numbers
+    .Any(pairedNumber => number + pairedNumber == target));
 }
+
+static int FindMatch(IOrderedEnumerable<int> numbers, int target) {
+    return numbers.FirstOrDefault(number => numbers
+    .Any(pairedNumber => number + pairedNumber == target));
+}
+

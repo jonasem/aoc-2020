7710e1ec9200497afe0631193c9ef4404bb942c0
jonas <sirjonas@gmail.com>
2020-12-16 20:36:05 +0100

    Day 3 - 1

--- Program.cs
+++ Program.cs
@@ -3,27 +3,15 @@ using System.IO;
 using System.Linq;
 
 
-var lines = File.ReadAllLines("input/day2");
-var passwordLines = lines.Select(ParseLine);
+var patternLines = File.ReadAllLines("input/day3");
+var patternWidth = patternLines.First().Length;
 
-Console.WriteLine("Day 2");
-Console.WriteLine(passwordLines.Where(PassesPolicy2).Count());
+var currentPosition = 0;
 
-static bool PassesPolicy(PasswordLine line) {
-    var hits = line.password.Where(character => character == line.letter).Count();
-    return line.min <= hits && line.max >= hits;
-}
+var total = patternLines.Skip(1).Aggregate(0, (sum, line) => {
+    currentPosition = currentPosition + 3;
+    var hit = line[currentPosition % patternWidth] == '#';
+    return (hit ? 1 : 0) + sum;
+});
 
-static bool PassesPolicy2(PasswordLine line) {
-    return line.letter == line.password[line.min - 1] ^line.letter == line.password[line.max - 1];
-}
-
-
-static PasswordLine ParseLine(string line) {
-    var parts = line.Split(" ");
-    var limits = parts[0].Split("-").Select(part => Convert.ToInt32(part)).ToArray();
-    return new PasswordLine(limits[0], limits[1], parts[1].First(), parts[2]);
-}
-
-record PasswordLine(int min, int max, char letter, string password);
-
+Console.WriteLine(total);

7462dfdb881ea76a074cdcfeca26b25527ea9073
jonas <sirjonas@gmail.com>
2020-12-16 21:02:24 +0100

    Day3 - 2

--- Program.cs
+++ Program.cs
@@ -4,14 +4,22 @@ using System.Linq;
 
 
 var patternLines = File.ReadAllLines("input/day3");
-var patternWidth = patternLines.First().Length;
 
-var currentPosition = 0;
-
-var total = patternLines.Skip(1).Aggregate(0, (sum, line) => {
-    currentPosition = currentPosition + 3;
-    var hit = line[currentPosition % patternWidth] == '#';
-    return (hit ? 1 : 0) + sum;
-});
+var slopes = new [] {(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)};
+var total = slopes.Aggregate(1L, (product, slope) => product * slopeHits(patternLines, slope.Item1, slope.Item2));
 
 Console.WriteLine(total);
+
+static int slopeHits(string[] patternLines, int verticalIncrement, int horizontalIncrement) {
+    var horizontalPosition = 0;
+    var patternWidth = patternLines.First().Length;
+
+    return patternLines
+        .Skip(verticalIncrement)
+        .Where((_, index) => index % verticalIncrement == 0)
+        .Aggregate(0, (sum, line) => {
+            horizontalPosition += horizontalIncrement;
+            var hit = line[horizontalPosition % patternWidth] == '#';
+            return (hit ? 1 : 0) + sum;
+        });
+}
